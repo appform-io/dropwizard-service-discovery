8aefb394b05b93fcff43aab85e3038562d61cebc
Koushik Ramachandra <rkoushik.14@gmail.com>
2023-03-06 12:52:22 +0530

    a) Added a log for when region is null. b) Added javadoc to the class

--- src/main/java/io/appform/dropwizard/discovery/bundle/resolvers/DefaultNodeInfoResolver.java
+++ src/main/java/io/appform/dropwizard/discovery/bundle/resolvers/DefaultNodeInfoResolver.java
@@ -3,8 +3,11 @@ package io.appform.dropwizard.discovery.bundle.resolvers;
 import io.appform.dropwizard.discovery.bundle.ServiceDiscoveryConfiguration;
 import io.appform.ranger.common.server.ShardInfo;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 
 @NoArgsConstructor
+@Slf4j
 public class DefaultNodeInfoResolver implements
   NodeInfoResolver {
 
@@ -12,9 +15,13 @@ public class DefaultNodeInfoResolver implements
 
   @Override
   public ShardInfo node(ServiceDiscoveryConfiguration configuration) {
+    val region = System.getenv(FARM_ID);
+    if(null == region){
+      log.debug("The value of the env variable FARM_ID is null. Setting the region to null on node data");
+    }
     return ShardInfo.builder()
       .environment(configuration.getEnvironment())
-      .region(System.getenv(FARM_ID))
+      .region(region)
       .tags(configuration.getTags())
       .build();
   }
